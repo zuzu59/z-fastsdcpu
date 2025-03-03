e914d7a158f653f7bf803e585a60567bcb67f067
Christian Zufferey <christian@zufferey.com>
2025-03-03 17:58:03 +0100

    new: terminé le script install.sh, et il fonctionne bien \o/

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 #Petit script pour installer facilement Fastsdcpu dans un container LXC sous Proxmox et ses outils qui vont bien
-#zf250303.1744
+#zf250303.1757
 
 # source: 
 
-echo -e "\Installation de Fastsdcpu..."
+echo -e "\Installation de Fastsdcpu...
 
+ATTENTION: il faut choisir le template Ubuntu 22.x standard pour le container LXC !
+La version Ubuntu supérieure ne fonctionne pas encore avec les libs de Fastsdcpu.
 
+"
 
+echo ""
+read -p "Etes-vous certain de continuer ( CTRL+C pour arrêter ) ?"
+echo ""
 
-
+apt update ; apt install python3.10-venv libgl1 socat
+mkdir  ~/dev ; cd ~/dev
+git clone git@github.com:rupeshs/fastsdcpu.git
+cd fastsdcpu/
+chmod +x install.sh 
+./install.sh 
