34f7e3c00198b294967640c472f274a5d111d805
Christian Zufferey <christian@zufferey.com>
2025-03-03 17:45:12 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Petit script pour installer facilement Fastsdcpu dans un container LXC sous Proxmox et ses outils qui vont bien
+#zf250303.1744
+
+# source: 
+
+echo -e "\Installation de Fastsdcpu..."
+
+
+
+
+
