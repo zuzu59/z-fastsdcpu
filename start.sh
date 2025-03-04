017e33be6fee055ef0719898ae94c0d82cffed8d
Christian Zufferey <christian@zufferey.com>
2025-03-04 14:52:17 +0100

    change: essayé d'améliorer le start, mais ce n'est pas encore ça

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour démarrer facilement Fastsdcpu dans un container LXC sous Proxmox, avec surtout la redirection du port sur le local network
-#zf250303.1859
+#zf250303, zf250304.1432
 
 # source: 
 
@@ -8,7 +8,25 @@ echo -e "Démarrage de Fastsdcpu...
 
 "
 
+./stop.sh
+
+
+#cp ./share-start.sh ../fastsdcpu/
+
 cd ~/dev/fastsdcpu/
 socat TCP-LISTEN:8080,fork,reuseaddr TCP:127.0.0.1:7860 &
+#./share-start.sh
 ./start-webui.sh &
+
+# > /tmp/fastsdcpu.log 2>&1 &
+
+exit
+
+echo -e "
+
+Fastsdcpu démarré !
+
+"
+
+#tail -f /tmp/fastsdcpu.log
 
