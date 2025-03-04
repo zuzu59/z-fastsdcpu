48b352122673f83c3fb9dfac4012e725c6842673
Christian Zufferey <christian@zufferey.com>
2025-03-04 14:52:53 +0100

    fix: corrigé le bug de quand il y a plusieurs 'start' à tuer

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Petit script pour arrêter facilement Fastsdcpu dans un container LXC sous Proxmox, qui a été démarré avec le start.sh !
-#zf250303.1858
+#zf250303, zf250304.1105
 
 # source: 
 
@@ -8,5 +8,14 @@ echo -e "Arrêt de Fastsdcpu...
 
 "
 
-pkill -P $(pgrep -f start)
+for pid in $(pgrep -f start); do
+    pkill -P $pid
+done
+
 pkill -f socat
+
+echo -e "
+
+Fastsdcpu arrêté !
+
+"
